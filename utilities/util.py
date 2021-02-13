882200eb43b492d462288bca141311b2f69818a5
Keshav Chakrapani <keshavc@gmail.com>
2021-02-12 22:21:48 -0800

    Hash func added

--- utilities/util.py
+++ utilities/util.py
@@ -1,4 +1,5 @@
 import json
+import secrets
 
 # Usage: Take entire trefle response and just slap it into json_trim.
 
@@ -13,10 +14,13 @@ def json_trim(response):
     return ret
 
 
-def test():
+def test_trim():
     json_path = 'example_response.json'
     with open(json_path, encoding="utf-8") as f:
         data = json.load(f)
     print(json_trim(data))
 
+
+def gen_hash():
+    return secrets.token_urlsafe(16)
 
