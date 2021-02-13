0580bbba9d664c45dd863c62c354c6f45002c6de
vivekfernz <vjf2018@gmail.com>
2021-02-13 14:15:01 -0500

    Updated SQLite for carbon footprint update

--- database.py
+++ database.py
@@ -23,20 +23,22 @@ def insert_user(hasher, name):
                   {'ID': hasher, 'username': name, 'user_plants': "", 'plant_count': 0})
 
 
-def insert_plant(hasher, name, plant, add_plants):
+def insert_plant(hasher, name, plant, add_plants, carb):
     with conn:
         c.row_factory = lambda cursor, row: row[0]
         c.execute("SELECT user_plant_count FROM users WHERE username=?", (name,))
         a = c.fetchall()[0] + add_plants
         c.execute("SELECT plant_count FROM gardens WHERE ID=?", (hasher,))
         d = c.fetchall()[0] + add_plants
-        c.execute("SELECT user_plants FROM users WHERE username=?",(name,))
+        c.execute("SELECT user_plants FROM users WHERE username=?", (name,))
         b = c.fetchall()[0] + " " + plant
+        c.execute("SELECT carb_footprint FROM gardens WHERE ID=?", (hasher,))
+        e = c.fetchall()[0] + carb
         print(b)
         c.row_factory = None
         c.execute("UPDATE users SET user_plant_count=?, user_plants=? WHERE username=?",(a, b, name))
-        c.execute("UPDATE gardens SET plant_count=? WHERE ID =  ?",(d, hasher))
-
+        c.execute("UPDATE gardens SET plant_count=? WHERE ID =  ?", (d, hasher))
+        c.execute("UPDATE gardens SET carb_footprint=? FROM gardens WHERe ID=?", (e, hasher))
 
 def get_users(hasher):
     with conn:
